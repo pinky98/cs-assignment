0216a61279cc3816a7b23590b699f6e58fe1853f
pinky98 <31351080+pinky98@users.noreply.github.com>
2017-08-25 20:49:12 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float a,b,c,d,e,f;
+cout<<"enter first number";
+cin>>a;
+cout<<"enter second number";
+cin>>b;
+c=a+b;
+cout<<"the sum is"<<c;
+d=a-b;
+cout<<"the difference is"<<d;
+e=a*b;
+cout<<"the product is"<<e;
+f=a/b;
+cout<<"the quotient is"<<f;
+return 0;
+}
+
