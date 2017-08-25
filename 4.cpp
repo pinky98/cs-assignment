af8c870902f385499bcd5e5a4c901d54c529efdf
pinky98 <31351080+pinky98@users.noreply.github.com>
2017-08-25 20:49:30 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"enter lenth";
+cin>>a;
+cout<<"enter breadth";
+cin>>b;
+c=2*(a+b);
+cout<<"the perimeter  is"<<c;
+return 0;
+}
+
