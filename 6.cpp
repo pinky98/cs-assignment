b4f53ce4b477db16f13025968b74e64a6f6d0438
pinky98 <31351080+pinky98@users.noreply.github.com>
2017-08-25 20:50:10 +0530

    Add files via upload

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c,d;
+cout<<"enter radius";
+cin>>a;
+b=2*a;
+c=3.14*a*a;
+d=2*3.14*a;
+cout<<"the diameter is"<<b;
+cout<<"the area is"<<c;
+cout<<"the circumference is"<<d;
+return 0;
+}
+
