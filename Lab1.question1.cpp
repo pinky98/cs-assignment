29c18181a7adff940c7c9355ef6102808f121045
pinky98 <31351080+pinky98@users.noreply.github.com>
2017-08-25 21:30:02 +0530

    Add files via upload

---
+++ Lab1.question1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int number = 54;
+	float decimal = 39.494784;
+	bool statement = false;
+	char character = 'j';
+	
+	cout << number << endl << decimal << endl << statement << endl << character;
+	
+	return 0;
+}
