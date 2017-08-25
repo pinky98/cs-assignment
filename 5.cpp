331ffcd103a009001d41888248ac4d7768db73de
pinky98 <31351080+pinky98@users.noreply.github.com>
2017-08-25 20:49:52 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"enter length";
+cin>>a;
+cout<<"enter breadth";
+cin>>b;
+c=a*b;
+cout<<"the area is"<<c;
+return 0;
+}
+
